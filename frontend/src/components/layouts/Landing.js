7ffbfde79b3bd91b524340dc5148411a35ecc437
Aryan Saurav <75740236+ARYANSAURAV16@users.noreply.github.com>
2023-05-05 18:38:06 +0530

    Update Landing.js

--- frontend/src/components/layouts/Landing.js
+++ frontend/src/components/layouts/Landing.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 const Landing = () => {
   return (
-    <div className='landing mb-sm-5'>
-      <div className='mt-5'>
-        <h2 className='logo text-center mb-md-4'>Alles Insurance</h2>
+    <div className='landing`'>
+    <h2 className='logo text-center mb-md-4'>INSACURE</h2>
         <h1 className='motto text-center '>We Promise To Keep You Alright</h1>
         <p className='saying text-center mt-md-3'>
           Instant everything. Great price. Big heart.
         </p>
+      <div className='landing mb-sm-5'>
+        
       </div>
     </div>
   );
